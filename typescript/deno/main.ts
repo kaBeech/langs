02c33487d9f5d436046215426e58dabe76b98054
kaBeech <contact@kaBeech.com>
2024-12-14 10:20:36 -0800

    Modularize

--- typescript/deno/main.ts
+++ typescript/deno/main.ts
@@ -1,7 +1,4 @@
-export const add = (a: number, b: number): number => {
-  return a + b;
-}
-
+import { add } from "./src/add.ts";
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   console.log("Add 2 + 3 =", add(2, 3));
